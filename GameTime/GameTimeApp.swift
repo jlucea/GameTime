00ec8630be755f240581e8697e9ec1f131926d15
Jaime Lucea <jaime.lucea@atactic.io>
2022-10-15 18:42:36 +0200

    Initial Commit

---
+++ GameTime/GameTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTimeApp.swift
+//  GameTime
+//
+//  Created by Jaime Lucea on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GameTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
