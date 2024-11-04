94eeefff218bda6832e53081d16038d19d0fc489
Jaime Lucea <jlucea@eservicios.indra.es>
2024-11-04 18:49:02 +0100

    ColorPickerView - Initial commit

---
+++ GameTime/View/Components/ColorPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerView.swift
+//  GameTime
+//
+//  Created by Jaime Lucea on 4/11/24.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorPickerView()
+}
