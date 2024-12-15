3554c8fde76018cfcc30a8dcbbdd6bc6a1baeb42
Jaime Lucea <jlucea@eservicios.indra.es>
2024-12-15 12:08:19 +0100

    TimerRows
    Introduced TimerRows. The MainView now displays non-active timers as a list in phone screens. This replaces the CardViews, which are still used on pad screen types.

---
+++ GameTime/View/Components/TimerRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerRowView.swift
+//  GameTime
+//
+//  Created by Jaime Lucea on 10/12/24.
+//
+
+import SwiftUI
+
+struct TimerRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerRowView()
+}
