c5dc04df73b99b152557d4cbbf718e1272938829
Jaime Lucea <jlucea@eservicios.indra.es>
2024-12-21 18:25:20 +0100

    Toolbar - Added Preview, cleaned up code

--- GameTime/View/Toolbar/GameTimeToolbar.swift
+++ GameTime/View/Toolbar/GameTimeToolbar.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct GameTimeToolbar {
         
+    /// The title displayed in the center of the toolbar.
     private static let toolbarTitle = "GameTime"
     
+    /**
+     Generates the toolbar content for the `GameTime` application.
+     
+     This method provides:
+     - A button to open a view for adding new timers.
+     - The app's title displayed in the center of the navigation bar.
+     - An edit button for managing existing timers (visible only when timers exist).
+     
+     - Parameters:
+       - showAddNewTimerScreen: A binding to control the visibility of the add timer screen.
+       - controller: The `GTTimerManager` instance used to manage timers and provide context for the toolbar.
+     - Returns: A `ToolbarContent` instance with the configured toolbar items.
+     */
     static func content(showAddNewTimerScreen: Binding<Bool>, controller: GTTimerManager) -> some ToolbarContent {
         Group {
             ToolbarItem(placement: .primaryAction) {
@@ -31,8 +45,7 @@ struct GameTimeToolbar {
             }
             ToolbarItem(placement: .navigationBarLeading) {
                 if !controller.timers.isEmpty {
-                    // Pressing this button will activate editMode
-                    EditButton()
+                    EditButton()       // Tapping this button will activate editMode
                 }
             }
         }
@@ -40,8 +53,15 @@ struct GameTimeToolbar {
     
 }
 
+// MARK: - Preview
+
 #Preview {
-    
-//    GameTimeToolbar(showAddNewTimerScreen: Binding.constant(false))
-//        .environmentObject(GTTimerManager())
+    NavigationView {
+        VStack {
+            Text("Content Area")
+        }
+        .toolbar {
+            GameTimeToolbar.content(showAddNewTimerScreen: Binding.constant(false), controller: GTTimerManager())
+        }
+    }
 }
